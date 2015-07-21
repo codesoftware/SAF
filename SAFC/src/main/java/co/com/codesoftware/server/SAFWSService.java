d70d1add60ed52281fe5381727bf2b7770a5b935
siscomputo <johnmorenoing@gmail.com>
2015-07-21 16:03:00 -0500

    cambio bootstrap

--- SAFC/src/main/java/co/com/codesoftware/server/SAFWSService.java
+++ SAFC/src/main/java/co/com/codesoftware/server/SAFWSService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "SAFWSService", targetNamespace = "http://server.codesoftware.com.co/", wsdlLocation = "http://192.168.0.13:9080/WS/server/SAFC?wsdl")
+@WebServiceClient(name = "SAFWSService", targetNamespace = "http://server.codesoftware.com.co/", wsdlLocation = "http://192.168.1.128:7080/WS/server/SAFWS?wsdl")
 public class SAFWSService
     extends Service
 {
@@ -30,7 +30,7 @@ public class SAFWSService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://192.168.0.13:9080/WS/server/SAFC?wsdl");
+            url = new URL("http://192.168.1.128:7080/WS/server/SAFWS?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
