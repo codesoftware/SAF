cda91075a7699cdb42a93f3b5d6c08b526212072
jnsierra <jnsierrac@gmail.com>
2015-08-17 21:13:28 -0500

    17/08/2015 006
    Consulta de facturacion

--- SAFC/src/main/java/co/com/codesoftware/server/SAFWSService.java
+++ SAFC/src/main/java/co/com/codesoftware/server/SAFWSService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.2
  * 
  */
-@WebServiceClient(name = "SAFWSService", targetNamespace = "http://server.codesoftware.com.co/", wsdlLocation = "http://192.168.0.9:9999/WS/server/SAFWS?wsdl")
+@WebServiceClient(name = "SAFWSService", targetNamespace = "http://server.codesoftware.com.co/", wsdlLocation = "http://192.168.0.20:9999/WS/server/SAFWS?wsdl")
 public class SAFWSService
     extends Service
 {
@@ -30,7 +30,7 @@ public class SAFWSService
         URL url = null;
         WebServiceException e = null;
         try {
-            url = new URL("http://192.168.0.9:9999/WS/server/SAFWS?wsdl");
+            url = new URL("http://192.168.0.20:9999/WS/server/SAFWS?wsdl");
         } catch (MalformedURLException ex) {
             e = new WebServiceException(ex);
         }
